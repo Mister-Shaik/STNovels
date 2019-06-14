6ac18bf280f2483047de5c2e07d240c958261feb
Shaik <mr.shaiksha@gmail.com>
2019-06-14 14:34:09 +0530

    Name generator API with post request

--- node/server.js
+++ node/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const names = require('./db/names-db.json');
 
 const app = express();
 
@@ -10,9 +11,31 @@ app.get('/', (req, res) => {
     res.send("hello")
 });
 
-app.post('/character/gen',(req, res) => {
-    console.log(req.body);
-    res.send("ok")
+//Name Generator
+app.post('/character/gen/:category',(req, res) => {
+
+    const collection = names[req.params.category];
+    const len = Object.keys(collection).length;
+    
+    //generate random first name and last name
+    const FirstName = Math.floor(Math.random() * len);
+    const LastName = Math.floor(Math.random() * len);
+    
+    let result = {};
+
+    //if chinese then add meaning to object
+    if(req.params.category == 'boy-c' || req.params.category == 'girl-c')
+    {        
+        result.name = collection[FirstName].name + " " + collection[LastName].name;
+        result.meaning = collection[FirstName].meaning == collection[LastName].meaning?collection[LastName].meaning : collection[FirstName].meaning + ' /// ' + collection[LastName].meaning;
+    }
+
+    else{
+        result.name = collection[FirstName].name + " " + collection[LastName].name;
+    }
+
+    //send object as response
+    res.send(result);
 });
 
 
