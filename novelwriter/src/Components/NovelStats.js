c7371925273abd10a1be45dcae7b7f777d12c4af
shaik <mr.shaiksha@gmail.com>
2019-09-15 16:26:28 -0700

    starting editing again

--- novelwriter/src/Components/NovelStats.js
+++ novelwriter/src/Components/NovelStats.js
@@ -4,7 +4,7 @@ class NovelStats extends Component {
     render() {
         return (
             <div className="pt2 pb2 ph4 flex flex-wrap justify-content-between">
-                <div className="w-30 pv4 ph3 br2 card mr3 mv3">
+                <div className="w-30 pt4 pb2 ph3 br2 card mr3 mv3">
                     <div className="pv2">
                         <div className="fr">
                             <select className="select-css">
@@ -16,11 +16,11 @@ class NovelStats extends Component {
                         <p>WORD COUNT</p>
                     </div>
                     <div className="tc">
-                        <h2 className="fl tr pv2">64</h2>
+                        <h2 className="f2 pv2">64</h2>
                         <p className="fr pt4">0.0%</p>
                     </div>
                 </div>
-                <div className="w-30 pv4 ph3 br2 card mr3 mv3">
+                <div className="w-30 pt4 pb2 ph3 br2 card mr3 mv3">
                     <div className="pv2">
                         <div className="fr">
                             <select className="select-css">
@@ -32,20 +32,20 @@ class NovelStats extends Component {
                         <p>TOTAL CHAPTERS</p>
                     </div>
                     <div className="tc">
-                        <h2 className="fl tr pv2">8</h2>
+                        <h2 className="f2 pv2">8</h2>
                         <p className="fr pt4">0.0%</p>
                     </div>
                 </div>
-                <div className="w-30 pv4 ph3 br2 card mr3 mv3">
+                <div className="w-30 pt4 pb2 ph3 br2 card mr3 mv3">
                     <div className="pv2">
                         <p>Avg WORDS / CH</p>
                     </div>
                     <div className="tc">
-                        <h2 className="fl tr pv2">232</h2>
+                        <h2 className="f2 pv2">232</h2>
                         <p className="fr pt4">0.0%</p>
                     </div>
                 </div>
-                <div className="w-30 pv4 ph3 br2 card mr3 mv3">
+                <div className="w-30 pt4 pb2 ph3 br2 card mr3 mv3">
                     <div className="pv2">
                         <div className="fr">
                             <select  className="select-css">
@@ -58,6 +58,7 @@ class NovelStats extends Component {
                     </div>
                     <div className="pv2 tc">
                         <h2>4 <span className="f3">hrs</span> 43 <span className="f3">mins</span></h2>
+                        <p className="gray fr pt4">?</p>
                     </div>
                 </div>
             </div>
