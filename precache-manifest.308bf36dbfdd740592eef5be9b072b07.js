c01f68d7172d3117086c226de8d2988627931c5d
shaiksha2195 <mr.shaiksha@gmail.com>
2020-01-14 16:38:47 +0400

    Updates

---
+++ precache-manifest.308bf36dbfdd740592eef5be9b072b07.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72b14ef7cef8c1ccad89c44e30b2e394",
+    "url": "/novelwriter/index.html"
+  },
+  {
+    "revision": "8c006d379c76b06cd6ca",
+    "url": "/novelwriter/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "5bb910a7e71fcde07c5f",
+    "url": "/novelwriter/static/css/main.67114a9d.chunk.css"
+  },
+  {
+    "revision": "8c006d379c76b06cd6ca",
+    "url": "/novelwriter/static/js/2.6db17143.chunk.js"
+  },
+  {
+    "revision": "c6d5a2a648e3cd37e6446cab570d306e",
+    "url": "/novelwriter/static/js/2.6db17143.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5bb910a7e71fcde07c5f",
+    "url": "/novelwriter/static/js/main.4be6fa91.chunk.js"
+  },
+  {
+    "revision": "5d767b28ce3e0775c1f2",
+    "url": "/novelwriter/static/js/runtime-main.1b176558.js"
+  }
+]);
